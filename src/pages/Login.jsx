33e6b6588e18e353e6c3035fa320c03d6876e325
andres agudelo <echeverry11298@gmail.com>
2022-11-01 15:28:18 -0500

    login creado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,20 @@ import Formulario from "../components/login/Formulario";
 import Links from "../components/login/Links";
 
 function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-            <h1>Bienvenidos sedan</h1>
-            <Links/>
-            <Formulario nombre="nati"/>
-        </div>
-    )
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900 h-screen">
+      <div className="flex flex-col items-center justify-center px-6 py-7 mx-auto md:h-screen lg:h-screen sm:h-screen" >
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img className="w-8 h-8 mr-2" src="src\assets\user.png" alt="logo" />
+          Sistema GEUV
+        </a>
+        <Formulario />
+      </div>
+    </section>
+  );
 }
 
 export default Login;
