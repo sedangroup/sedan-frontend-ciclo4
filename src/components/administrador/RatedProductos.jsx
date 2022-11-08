d88a0b958a0b42b2c6df67449b1f1e576f0b43a0
andres agudelo <echeverry11298@gmail.com>
2022-11-08 15:05:56 -0500

    ordenamos los productos por calificacion

--- src/components/administrador/RatedProductos.jsx
+++ src/components/administrador/RatedProductos.jsx
@@ -2,13 +2,14 @@ import StarRating from "./StarRating";
 import { productos } from "./data";
 
 function RatedProductos() {
+  let sortedProducts = productos.sort(function(a, b){return a.rating-b.rating;}).reverse();
   return (
     <div className="h-full w-full overflow-auto">
       <h2 className="text-lg text-center font-bold">
         Productos mejor valorados
       </h2>
       <ul className=" p-3 overflow-auto  grid grid-cols-1 divide-y ">
-        {productos?.sort((val)=>val.price<val.price).map((prod, key) => (
+        {sortedProducts?.map((prod, key) => (
           <li className="flex gap-2 items-center justify-center p-1" key={key}>
             <img
               src={prod.img}
